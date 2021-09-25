4f1f8496066e86bedb6174e8d20805caf7fc182f
Cutler Sheridan <cutler.sheridan@gmail.com>
2021-09-24 17:03:29 -0700

    Make books reflow on window resize

--- README.md
+++ README.md
@@ -1,15 +1,23 @@
 # A bookshelf
 
 ##### TO-DO NEXT
+- fuck should i just redo all the shelves with flex or is it worth the facility of having the shelf underlines in their proper, exact places?
 
 ##### TO-DO LATER
+- figure out why the books will go like 6px into the right padding
+- add max- and min-width to books
 - add .read and .notRead classes to style differently somehow
+- shorten author and title names by abbreviating probably
 - handle long titles/authors
+- by default, organize by author?
 - start with x(3?) shelves then add more as needed
 - sort them different ways
 - add genres?
+- how to make things draggable?  not necessary but interesting question
 
 ###### DONE
+- currently, smaller books will fill in gaps left by big books on earlier rows; decide if I want it this way - could be good, or maybe not if I want to keep authors together.  in fact could make filtering harder in general if they're not in a specific order
+- reflow books on shelves upon window resize
 - make book card width correspond to page count
 - learned that arrow functions negate "this" use in function declaration
 - figured out new objects have to be instantiated *after* the object prototype methods are defined
--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ function Book(title, author, pages, hasRead) {
     this.author = author;
     this.pages = pages;
     this.hasRead = hasRead;
-    this.displayedOnShelf = false;
+    addBookToBookshelf(this);
 }
 Book.prototype = {
     info: function() {
@@ -13,57 +13,105 @@ Book.prototype = {
     }
 }
 
-const shelf1 = document.querySelector(".shelf1");
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 325, true);
-const dune = new Book("Dune", "Frank Herbert", 880, true);
-const hp4 = new Book("Harry Potter and the Goblet of Fire", "J.K. Rowling", 600, true);
-const theStand = new Book("The Stand", "Stephen King", 1200, false);
+let currentShelf = 0;
 
+const dune = new Book("Dune", "Frank Herbert", 798, true);
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 366, true);
+const hp4 = new Book("Harry Potter and the Goblet of Fire", "J.K. Rowling", 734, true);
+const theStand = new Book("The Stand", "Stephen King", 1152, false);
+const pps = new Book("Perdido Street Station", "China Miéville", 710, true);
+const later = new Book("Later", "Stephen King", 248, true);
+const mrNorrell = new Book("Jonathan Strange & Mr. Norrell", "Susanna Clarke", 1006, true);
+const neuromancer = new Book("Neuromancer", "William Gibson", 292, false);
+
+// this may not need to be a spread if it's only used for singular new books + the constructor
+function addBookToBookshelf(...books) {
+    books.forEach(book => {
+        if (!myBookshelf.includes(book)) {
+            myBookshelf.push(book);
+        }
+    });
+}
+updateBooksDisplay();
 /*--MODEL END--*/
 
 /*--CONTROL START--*/
-function addBookToBookshelf(...books) {
-    books.forEach(book => myBookshelf.push(book));
-}
-console.log(theHobbit.info());
-addBookToBookshelf(dune, theHobbit, hp4, theStand);
-console.table(myBookshelf);
-
+// logic
 /*--CONTROL END--*/
 
 /*--VIEW START--*/
-// maybe this whole function should go within addBookToBookshelf.
-// removes need for displayedOnShelf
-function displayBooks() {
-    // this might make more sense as a for loop starting at the
-    // end of myBookshelf, counting down, and breaking once it reaches
-    // an already-displayed book
-    myBookshelf.forEach(book => {
-        if (book.displayedOnShelf === true) {
-            return;
-        }
+window.addEventListener("resize", updateBooksDisplay);
 
-        const newBook = document.createElement("div");
-        newBook.classList.add("book");
-        newBook.style.width = `${book.pages / 60}rem`;
-        const bookTitle = document.createElement("div");
-        bookTitle.classList.add("title");
-        bookTitle.textContent = book.title;
-        const titleUnderline = document.createElement("hr");
-        titleUnderline.classList.add("title-underline");
-        const bookAuthor = document.createElement("div");
-        bookAuthor.classList.add("author");
-        bookAuthor.textContent = book.author;
-        const bookPages = document.createElement("div");
-        bookPages.classList.add("pages");
-        bookPages.textContent = `${book.pages} pages`;
-        const bookInfo = [bookTitle, titleUnderline, bookAuthor, bookPages];
-        newBook.append(...bookInfo);
-        //newBook.textContent = book.info();
-
-        shelf1.appendChild(newBook);
-        book.displayedOnShelf = true;
+function updateBooksDisplay() {
+    // clear the bookshelf
+    currentShelf = 0;
+    clearShelves();
+    myBookshelf.forEach(book => createBookElement(book));
+}
+function clearShelves() {
+    const shelves = document.querySelectorAll(".shelves");
+    shelves.forEach(shelf => {
+        const booksOnShelf = document.createRange();
+        booksOnShelf.selectNodeContents(shelf);
+        booksOnShelf.deleteContents();
     })
 }
-displayBooks();
+function createBookElement(book) {
+    const bookElement = document.createElement("div");
+    bookElement.classList.add("book");
+    bookElement.style.width = `${book.pages / 60}rem`;
+    const bookTitle = document.createElement("div");
+    bookTitle.classList.add("title");
+    bookTitle.textContent = book.title;
+    const titleUnderline = document.createElement("hr");
+    titleUnderline.classList.add("title-underline");
+    const bookAuthor = document.createElement("div");
+    bookAuthor.classList.add("author");
+    bookAuthor.textContent = book.author;
+    const bookPages = document.createElement("div");
+    bookPages.classList.add("pages");
+    bookPages.textContent = `${book.pages} pages`;
+
+    const bookInfo = [bookTitle, titleUnderline, bookAuthor, bookPages];
+    bookElement.append(...bookInfo);
+
+    calculateCurrentShelf(parseInt(bookElement.style.width) * 10);
+    console.log(`current shelf: ${currentShelf}`);
+    const shelfToUse = document.querySelector(`.shelf${currentShelf}`);
+    shelfToUse.appendChild(bookElement);
+}
+function calculateCurrentShelf(newBookWidth) {
+    // using a flex would be sooooo much more performant
+    const shelves = document.querySelectorAll(".shelves");
+    const shelfStyle = getComputedStyle(shelves[0]);
+    const shelfPadding = parseInt(shelfStyle.paddingLeft) + parseInt(shelfStyle.paddingRight);
+    const shelfWidth = parseInt(shelfStyle.width) - shelfPadding;
+    const bookGap = parseInt(shelfStyle.gap);
+    console.log("\ninside calculateCurrent");
+
+    for (let i = currentShelf; i < shelves.length; i++) {
+        const booksOnShelf = document.querySelectorAll(`.shelf${i} > *`);
+        let booksOnShelfWidth = 0;
+        let booksGapSum = 0;
+        booksOnShelf.forEach(bookElement => {
+            const bookWidth = parseInt(getComputedStyle(bookElement).width);
+            booksOnShelfWidth += bookWidth;
+            //console.log(`bookWidth: ${bookWidth}`);
+            console.log(bookElement.textContent);
+        });
+
+        if (booksOnShelf.length > 1) {
+            booksGapSum = bookGap * (booksOnShelf.length - 1);
+        }
+        console.log(`shelf width: ${shelfWidth}`);
+        console.log(`booksOnShelfWidth: ${booksOnShelfWidth}`);
+        console.log(`newBookWidth: ${newBookWidth}`);
+        if (newBookWidth < (shelfWidth - (booksOnShelfWidth + booksGapSum))) {
+            currentShelf = i;
+            return;
+        }
+    }
+    currentShelf = 0;
+}
+
 /*--VIEW END--*/
